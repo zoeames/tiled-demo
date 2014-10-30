b8f0f57c09f89aa21cb5857ddb98cb7511936912
Zoe Ames <zoeames@gmail.com>
2014-10-30 17:20:55 -0500

    tiled

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(300, 320, Phaser.CANVAS, 'tiled', {preload:preload, create:create, update:update});
+var game = new Phaser.Game(600, 384, Phaser.CANVAS, 'tiled', {preload:preload, create:create, update:update});
 
 function preload(){
   game.load.tilemap('map', '/assets/tiled.json', null, Phaser.Tilemap.TILED_JSON);
@@ -8,8 +8,63 @@ function preload(){
   game.load.spritesheet('balls', '/assets/balls.png', 17, 17);
 }
 
+var map, background, scenery, plants, ground, dude, cursors, money;
+
 function create(){
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+
+  map = game.add.tilemap('map');
+  map.addTilesetImage('desert', 'desert');
+
+  background = map.createLayer('background');
+  scenery = map.createLayer('scenery');
+  plants = map.createLayer('plants');
+  ground = map.createLayer('ground');
+  //money = map.createLayer('money');
+  plants.resizeWorld();
+  map.setCollision(26, true, 'ground');
+
+
+  money = game.add.group();
+  money.enableBody = true;
+  money.physicsBodyType = Phaser.Physics.ARCADE;
+  map.createFromObjects('money', 49, 'coin', 0, true, false, money);
+
+  money.callAll('animations.add', 'animations', 'spin', [5, 4, 3, 2, 1, 0], 10, true);
+  money.callAll('animations.play', 'animations', 'spin');
+
+
+  dude = game.add.sprite(0, 0, 'dude');
+  dude.animations.add('left', [0, 1, 2, 3], 10, true);
+  dude.animations.add('right', [5, 6, 7, 8], 10, true);
+  game.physics.arcade.enable(dude);
+  dude.body.gravity.y = 300;
+  game.camera.follow(dude);
+  //dude.setAll('body.collideWorldBounds', true);
+
+  cursors = game.input.keyboard.createCursorKeys();
 }
 
 function update(){
-}
+  game.physics.arcade.collide(dude, ground);
+  if(cursors.left.isDown){
+    dude.body.velocity.x = -150;
+    dude.animations.play('left');
+  }
+  else if(cursors.right.isDown){
+    dude.body.velocity.x = 150;
+    dude.animations.play('right');
+  }else{
+    dude.body.velocity.x = 0;
+    dude.animations.stop();
+    dude.frame = 4;
+  }
+
+  if(cursors.down.isDown){
+    dude.body.velocity.y = 350;
+  }
+
+  if(cursors.up.isDown){
+    dude.body.velocity.y = -350;
+    }
+  }
